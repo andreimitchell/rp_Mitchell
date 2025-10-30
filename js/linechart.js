5b47a893e3a734ebe1499f943870b083cbc8ed91
andreimitchell <andreicmitchell2009@gmail.com>
2025-10-29 22:59:17 -0400

    did not work disregard

--- js/linechart.js
+++ js/linechart.js
@@ -1,7 +1,7 @@
 // Code for generating a Chart.js line chart
 
 async function getDataNQ() {
-    const response = await fetch('./data/nqdata.csv'); // . move up one folder
+    const response = await fetch('../data/nqdata.csv'); // .. move up one folder
     const data = await response.text()                      // CSV to TEXT format
 
 
@@ -276,7 +276,7 @@ createChartNQ();
 
 
 async function getDataES() {
-    const response = await fetch('./data/esdata.csv'); // . move up one folder
+    const response = await fetch('../data/esdata.csv'); // .. move up one folder
     const data = await response.text()                      // CSV to TEXT format
 
 
